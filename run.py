f03c23893b6451c91018db19b95a30e8a8e6eb45
Welles Robinson <wir963@gmail.com>
2017-10-02 12:27:03 -0400

    reorganized data and use the same order of columns when comparing signatures

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.metrics.pairwise import pairwise_distances, cosine_similarity
 
+import matplotlib.pyplot as plt
+
 # load the example signatures
 # example_signatures = np.load("data/examples/example-signatures.npy")
 # calculated_signatures = np.load("data/examples/calculated-signatures.npy")
@@ -14,10 +16,24 @@ def run_kmeans(X, N):
     kmeans.fit_predict(X)
     return kmeans
 
+# eliminate any columns that together add up to less than 1% of the total mutations
+def reduce_dimension(M):
+    fraction_per_category = M.sum(axis=0)/M.sum()
+    fraction_per_category.sort()
+    for i in range(1, 97):
+        # find the minimum category
+        if sum(fraction_per_category[:i]) > .01:
+            # we want to remove the smallest i-1 categories
+            vals_to_remove = fraction_per_category[:i-1]
+            cols_to_remove = np.isin(M.sum(axis=0)/M.sum(), vals_to_remove)
+            return np.logical_not(cols_to_remove)
+
+
 # for a given n, the number of signatures, calculate the signatures from M
 def calculate_signatures(N, M):
     # M is a numpy matrix where rows represent samples and columns represent mutation categories
-    # TODO dimension reduction - eliminate any columns that together add up to less than 1% of the total mutations
+    cols_to_keep = reduce_dimension(M)
+    M = M[:, cols_to_keep]
     # approach - find the min column, update the cumulative sum and if less than .01, remove, update and repeat
     E_list = [] # exposure_matrix_list
     P_list = [] # signature_matrix_list
@@ -61,10 +77,14 @@ def run_synthetic_data():
     N = 5
     M = np.loadtxt("data/examples/example-mutation-counts.tsv", dtype=np.dtype(np.int32), delimiter="\t", skiprows=1, usecols=list(range(1,21)))
     signatures = calculate_signatures(N, M)
-    np.save("data/examples/calculated-signatures-" + str(N) + ".npy", signatures)
+    np.save("output/9_29_17/signatures/synthetic-signatures-" + str(N) + ".npy", signatures)
     actual_signatures = np.load("data/examples/example-signatures.npy")
     # smaller pairwise distance means more similar
-    print(cosine_similarity(actual_signatures, signatures))
+    # M = cosine_similarity(actual_signatures, signatures)
+    # T = range(M.shape[0])
+    # for i in range(M.shape[1]):
+    #     plt.plot(T, M[:,i])
+    # plt.show()
 
 def get_real_data():
     # define a generator that strips the first column
@@ -98,22 +118,35 @@ def get_real_data():
     return(M)
 
 def run_real_data():
-    N_list = list(range(23, 30))
+    N_list = list(range(27, 28))
     for N in N_list:
         print(N)
         M = get_real_data()
         signatures = calculate_signatures(N, M)
-        np.save("data/signatures/calculated-signatures-" + str(N) + ".npy", signatures)
+        np.save("output/9_29_17/signatures/real-data-signatures-" + str(N) + ".npy", signatures)
 
 def analyze_signatures():
-    N_list = list(range(22, 25))
+    N_list = list(range(27, 28))
     for N in N_list:
         print("when N == " + str(N))
-        calculated_signatures = np.load("data/signatures/calculated-signatures-" + str(N) + ".npy")
-        cosmic_signatures = np.loadtxt("data/signatures/signatures.txt", dtype=np.dtype(np.float32), delimiter="\t", skiprows=1, usecols=list(range(3,25)))
+        # get the names of the mutation categories for the calculated signatures
+        cal_sig_mut_categories = np.loadtxt("data/whole_exome/ALL_exomes_mutational_catalog_96_subs.txt", dtype=str, skiprows=1, usecols=list(range(0,1)))
+        calculated_signatures = np.load("output/9_28_17/signatures/calculated-signatures-" + str(N) + ".npy")
+        cosmic_signatures = np.loadtxt("data/signatures/signatures.txt", dtype=np.dtype(np.float32), delimiter="\t", skiprows=1, usecols=list(range(3,30)))
         cosmic_signatures = cosmic_signatures.transpose()
+        cos_sig_mut_categories = np.loadtxt("data/signatures/signatures.txt", dtype=str, delimiter="\t", skiprows=1, usecols=list(range(2,3)))
+        # for each mutation category in the calculated signature, we want to get that column in the cosmic signatures
+        l = list()
+        for cal_sig_mut_cat in cal_sig_mut_categories:
+            # add the column to the list
+            l.append(cosmic_signatures[:, cos_sig_mut_categories == cal_sig_mut_cat])
+        cosmic_signatures = np.concatenate(l, axis=1)
         # print(pairwise_distances(calculated_signatures, cosmic_signatures, metric='cosine'))
-        print(cosine_similarity(calculated_signatures, cosmic_signatures))
+        M = cosine_similarity(calculated_signatures, cosmic_signatures)
+        T = range(M.shape[0])
+        for i in range(M.shape[1]):
+            plt.plot(T, M[:,i])
+        plt.show()
 
 
 if __name__ == '__main__':
